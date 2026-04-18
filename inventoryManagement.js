f8c0a46eadd8bf4cf3530bacfe0bbb029a40f85d
vanessamzuniga-beep <vanessamzuniga@gmail.com>
2026-04-17 23:37:38 -0500

    Inventory management lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,4 +1,36 @@
 // Write your code here
+// create product inventory array
+let products = ["Laptop", "Phone", "Headphones", "Monitor"]
+
+
+// Access product information
+ function logFirstProduct(){
+  console.log(products[0])
+ }
+
+
+// Add a product
+function addProduct(name) {
+  products.push(name)
+}
+addProduct("mouse")
+console.log(products[4])
+
+
+// Update product information
+function updateProductName(index, name){
+  products[index] = name
+}
+updateProductName(0,"computer")
+console.log(products)
+
+
+// Remove last product from array
+function removeLastProduct(){
+  products.pop()
+}
+removeLastProduct(products)
+console.log(products)
 
 
 
